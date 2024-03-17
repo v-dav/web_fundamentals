08b01ffc9bd29220f0bbb94e902fef688e78f378
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-17 17:32:29 +0100

    add exemple delete method

--- api/index.js
+++ api/index.js
@@ -10,6 +10,10 @@ app.get('/hello', (req, res) => {
 	res.send("hello there")
 })
 
+app.delete('/hello', (req, res) => {
+	res.send("I don't want to say hello to you")
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening to the port ${port}`)
 })
