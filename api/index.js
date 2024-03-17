5f1d860034ead5f7f858ebd86d40f0e7c084ae80
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-17 18:12:00 +0100

    add methods and post route to the api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 const express = require("express")
+let data = require("./data")
+
 const app = express()
 const port = 3000
+app.use(express.json())
 
+
+const addListing = (newItem) => {
+	data = [...data, newItem]
+}
+
+// GET /listings - return the full list of listings
+// POST /listings - create a new listing
+
+// GET /listings/id - return a specific listing matching an id
+// PUT /listings/id - update the specific listing matching the id
+// DELETE /listings/id - delete the listing matching the id from the list
+
+// Basic examples of the API
 app.get('/', (req, res) => {
 	res.send("Hello World")
 })
@@ -12,6 +28,16 @@ app.get('/hello', (req, res) => {
 
 app.delete('/hello', (req, res) => {
 	res.status(404).send("I don't want to say hello to you")
+})
+
+// Our listing exemple API
+app.get("/listings", (req, res) => {
+	res.send(data)
+})
+
+app.post("/listings", (req, res) => {
+	addListing(req.body)
+	res.status(201).send(data)
 })
 
 app.listen(port, () => {
