a50f3221d52af26fe219889125e1fc74a07d1659
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-17 18:11:40 +0100

    a moking database

---
+++ api/data.js
@@ -0,0 +1,25 @@
+const listing = [
+	{
+		id: 1,
+		header: "London 2-room appartement",
+		desciption: "Fine appartment in the center London",
+		price: 6000,
+		imageUrl: "https://somecdn.com/image"
+	},
+	{
+		id: 2,
+		header: "London studio",
+		desciption: "Fine appartment in the center London",
+		price: 6000,
+		imageUrl: "https://somecdn.com/image"
+	},
+	{
+		id: 3,
+		header: "London big house",
+		desciption: "Fine appartment in the center London",
+		price: 6000,
+		imageUrl: "https://somecdn.com/image"
+	}
+]
+
+module.exports = listing;
