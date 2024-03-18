f3c74190d546c3e5f0c2319e85203d3c9db9fd2c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-18 11:39:36 +0100

    render listing on front end part from the db

--- frontend/script.js
+++ frontend/script.js
@@ -16,7 +16,46 @@ renderHeader()
 const getListing = () => {
 	fetch("http://localhost:3000/listings")
 	.then(response => response.json())
-	.then(data => console.log(data))
+	.then(data => {
+		const listingsContainer = createListingsContainer()
+		data.forEach(listing => {
+			const listingNode = createListing(listing)
+			listingsContainer.appendChild(listingNode)
+		})
+		rootElement.appendChild(listingsContainer)
+	})
+}
+
+const createListingsContainer = () => {
+	const container = document.createElement("div")
+	container.classList.add("listings-container")
+	return container
+}
+
+const createListing = (listingData) => {
+	const listing = document.createElement("a")
+	listing.classList.add("listing")
+	const image = document.createElement("img")
+	image.src = listingData.imageUrl
+	
+	const header = document.createElement("h2")
+	header.classList.add("listing-header")
+	header.textContent = listingData.header
+	
+	const paragraph = document.createElement("p")
+	paragraph.classList.add("listing-paragraph")
+	paragraph.textContent = listingData.description
+	
+	const innerContainer = document.createElement("div")
+	innerContainer.classList.add("listing-inner-container")
+	
+	innerContainer.appendChild(header)
+	innerContainer.appendChild(paragraph)
+
+	listing.appendChild(image)
+	listing.appendChild(innerContainer)
+
+	return listing
 }
 
 getListing()
