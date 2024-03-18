06d0afa409df90af547d5c1ddadbc67193b30344
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-18 11:39:19 +0100

    add img links to the data mockup

--- api/data.js
+++ api/data.js
@@ -2,30 +2,23 @@ const listing = [
 	{
 		id: 1,
 		header: "London 2-room appartement",
-		desciption: "Fine appartment in the center London",
+		description: "Fine appartment in the center London",
 		price: 6000,
-		imageUrl: "https://somecdn.com/image"
+		imageUrl: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg"
 	},
 	{
 		id: 2,
 		header: "London studio",
-		desciption: "Fine appartment in the center London",
+		description: "Fine appartment in the center London",
 		price: 6000,
-		imageUrl: "https://somecdn.com/image"
+		imageUrl: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg"
 	},
 	{
 		id: 3,
 		header: "London big house",
-		desciption: "Fine appartment in the center London",
+		description: "Fine appartment in the center London",
 		price: 6000,
-		imageUrl: "https://somecdn.com/image"
-	},
-	{
-		id: 4,
-		header: "London huge house",
-		desciption: "Fine appartment in the center London",
-		price: 6000,
-		imageUrl: "https://somecdn.com/image"
+		imageUrl: "https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg"
 	}
 ]
 
